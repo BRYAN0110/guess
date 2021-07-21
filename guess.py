2bd5334b87fab34f46756718c12f91e09f52ea0f
Bryan Chen <bryan850110@gmail.com>
2021-07-21 17:16:41 +0800

    how many times do you guess

--- guess.py
+++ guess.py
@@ -1,8 +1,9 @@
 import random
 r = random.randint(1,100)
-
-while True:
+i = 1
+while i > 0:
 	figure = input('請猜一個數字:')
+	print('第', i , '次猜測')
 	if r == int(figure):
 		print('答對了!!!')
 		break
@@ -10,3 +11,4 @@ while True:
 		print('再大一點!')
 	else :
 		print('再小一點!')
+	i = i + 1
